1fb6d8da9273cc094a641332035df38d3276c64d
Carlos Fernández Mantilla <cefernandez7@espe.edu.ec>
2025-11-24 16:46:54 -0500

    implementación de Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    protected $fillable = ['title', 'content', 'user_id'];
+
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
