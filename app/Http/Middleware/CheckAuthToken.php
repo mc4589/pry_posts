7a5a35068fb8a8d09914637c565de036708de573
jrhernandez1991 <jrhernandez1@espe.edu.ec>
2025-11-24 17:45:06 -0500

    Update CheckAuthToken.php

--- app/Http/Middleware/CheckAuthToken.php
+++ app/Http/Middleware/CheckAuthToken.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facade\Http;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckAuthToken
@@ -15,6 +16,31 @@ class CheckAuthToken
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $token = $request->bearerToken();
+
+        if(!$token) {
+            return response()->json([''message => 'Token no proporcionado'], 401);
+        }
+
+        // URL del microservicio de autenticacion
+        $authUrl = rtrim(env('AUTH_SERVICE_URL', 'http://127.0.0.1:8000'), '/');
+
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer' . $token,
+        ])->get("{$authUrl}/api/validate-token");
+
+        if ($response->failed() || $response->status() !== 200) {
+            return response()->json(['message' => 'Token invalido o servicio disponible'], 401);
+        }
+        
+        $data = $response->json();
+
+        if (!isset($data['valid'] !== true) {
+            return response()->json(['message' => 'Token invalido expirado'], 401);
+        }
+        $request->attributes->set('auth_user', $data['user']);
+        
         return $next($request);
     }
 }
