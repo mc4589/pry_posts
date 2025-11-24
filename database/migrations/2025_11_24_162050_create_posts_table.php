7d2fae10cf8bb99e462c90b734168ef134d03da3
Jessy_Mariño <sjmarino1@espe.edu.ec>
2025-11-24 17:27:08 -0500

    Update 2025_11_24_162050_create_posts_table.php

--- database/migrations/2025_11_24_162050_create_posts_table.php
+++ database/migrations/2025_11_24_162050_create_posts_table.php
@@ -1,23 +1,23 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
-{
+    {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedBigInteger('user_id');
         });
     }
-
-    /**
+      /**
      * Reverse the migrations.
      */
     public function down(): void
@@ -25,3 +25,5 @@ return new class extends Migration
         Schema::dropIfExists('posts');
     }
 };
+   
+    
