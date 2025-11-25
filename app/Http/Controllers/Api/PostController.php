6c512963f6b9f403ac1a8d5d893eda9334495eb7
Eduardo Cantuña <118578087+EduardoCantunaCela@users.noreply.github.com>
2025-11-24 20:34:14 -0500

    Implement store method for Post creation
    Added a store method for creating new posts with validation.

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -14,4 +14,23 @@ class PostController extends Controller
         return response()->json(Post::with('user')->latest()->get());
     }
 
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'   => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        $user = $request->attributes->get('auth_user');
+
+        $post = Post::create([
+            'title'    => $request->title,
+            'content'  => $request->content,
+            'user_id'  => $user['id'],
+        ]);
+
+        return response()->json($post->load('user'), 201);
+    }
+
+
 }
