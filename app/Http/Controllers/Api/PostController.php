d9f1dd14e45f9dec6581faafc1b4810f668a98b5
Eduardo Cantuña <118578087+EduardoCantunaCela@users.noreply.github.com>
2025-11-24 20:33:47 -0500

    Add index method to PostController for JSON response

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,4 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Post::with('user')->latest()->get());
+    }
+
 }
